e5a923787201360d7d475b43be8812a229b34268
rraj77 <rajkumar.ahirwar@backbenchertechnologies.com>
2024-10-01 17:38:29 +0530

    enable cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['cdn.weatherapi.com', 'images.unsplash.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.weatherapi.com', 'images.unsplash.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/api.weatherapi.com', // Keep your provided source
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Use with caution
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+      {
+        source: '/api/api.unsplash.com', // Keep your provided source
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Use with caution
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
