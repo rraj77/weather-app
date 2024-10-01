87c2efe86fadd6220b6d8845190fe92d3f580b2b
rraj77 <rajkumar.ahirwar@backbenchertechnologies.com>
2024-10-01 20:08:45 +0530

    add loader

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,6 +12,7 @@ import { RootState } from '../store/store';
 import Highlights from '../components/Highlights';
 import Header from '../components/Header';
 import CityImage from '../components/CityImage';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const formatDate = (date: Date) => {
   const days = [
@@ -30,6 +31,7 @@ const formatDate = (date: Date) => {
 };
 
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const { current, unit } = useSelector((state: RootState) => state.weather);
 
@@ -37,37 +39,44 @@ export default function Dashboard() {
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position) => {
-      const { latitude, longitude } = position.coords;
-      const defaultCity = `${latitude},${longitude}`;
-      const weatherData = await fetchWeather(search ? search : defaultCity);
+      try {
+        setLoading(true);
+        const { latitude, longitude } = position.coords;
+        const defaultCity = `${latitude},${longitude}`;
+        const weatherData = await fetchWeather(search ? search : defaultCity);
 
-      const cityImage = await fetchCityImage(weatherData.location.name);
-      dispatch(
-        setWeatherData({
-          forecast: weatherData.forecast.forecastday,
-          current: {
-            icon: weatherData.current.condition.icon,
-            temp_c: weatherData.current.temp_c,
-            temp_f: weatherData.current.temp_f,
-            cityImage: cityImage,
-            uv: weatherData.current.uv,
-            wind_kph: weatherData.current.wind_kph,
-            wind_dir: weatherData.current.wind_dir,
-            humidity: weatherData.current.humidity,
-            vis_km: weatherData.current.vis_km,
-            weatherCondition:
-              weatherData.forecast.forecastday[0].day.condition.text,
-            rainChance:
-              weatherData.forecast.forecastday[0].day.daily_chance_of_rain,
-            location: {
-              country: weatherData.location.country,
-              city: weatherData.location.region,
-              name: weatherData.location.name,
+        const cityImage = await fetchCityImage(weatherData.location.name);
+        dispatch(
+          setWeatherData({
+            forecast: weatherData.forecast.forecastday,
+            current: {
+              icon: weatherData.current.condition.icon,
+              temp_c: weatherData.current.temp_c,
+              temp_f: weatherData.current.temp_f,
+              cityImage: cityImage,
+              uv: weatherData.current.uv,
+              wind_kph: weatherData.current.wind_kph,
+              wind_dir: weatherData.current.wind_dir,
+              humidity: weatherData.current.humidity,
+              vis_km: weatherData.current.vis_km,
+              weatherCondition:
+                weatherData.forecast.forecastday[0].day.condition.text,
+              rainChance:
+                weatherData.forecast.forecastday[0].day.daily_chance_of_rain,
+              location: {
+                country: weatherData.location.country,
+                city: weatherData.location.region,
+                name: weatherData.location.name,
+              },
             },
-          },
-          unit: 'c',
-        })
-      );
+            unit: 'c',
+          })
+        );
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     });
   }, [dispatch, search]);
 
@@ -81,83 +90,94 @@ export default function Dashboard() {
         padding: '4rem',
       }}
     >
-      <Paper elevation={0} sx={{ borderRadius: '25px' }}>
-        <Grid2 container columns={16}>
-          {current.icon && (
-            <Grid2 size={4.5} sx={{ padding: '3rem' }}>
-              <SearchBar setSearch={setSearch} />
+      {loading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <CircularProgress size="3rem" />
+        </Box>
+      ) : (
+        <Paper elevation={0} sx={{ borderRadius: '25px' }}>
+          <Grid2 container columns={16}>
+            {current.icon && (
+              <Grid2 size={4.5} sx={{ padding: '3rem' }}>
+                <SearchBar setSearch={setSearch} />
 
-              <Box textAlign="center" mt={4} mb={3}>
-                <Image
-                  src={`http:${current.icon}`}
-                  width={250}
-                  height={250}
-                  alt="image"
-                />
-              </Box>
-
-              <Box display="flex" mb={1.5}>
-                <Typography variant="h1" fontWeight={400}>
-                  {unit === 'c' ? current.temp_c : current.temp_f}
-                </Typography>
-                <Typography variant="h3">
-                  {unit === 'c' ? '°C' : '°F'}
-                </Typography>
-              </Box>
-
-              <Stack gap={3}>
-                <Box display="flex" gap="5px">
-                  <Typography>{day},</Typography>
-                  <Typography color="grey">{time}</Typography>
-                </Box>
-
-                <Divider />
-                <Box display="flex" alignItems="center" gap="10px">
+                <Box textAlign="center" mt={4} mb={3}>
                   <Image
                     src={`http:${current.icon}`}
-                    width={30}
-                    height={30}
-                    alt={current.weatherCondition}
+                    width={250}
+                    height={250}
+                    alt="image"
                   />
-                  <Typography fontSize="12px">
-                    {current.weatherCondition}
+                </Box>
+
+                <Box display="flex" mb={1.5}>
+                  <Typography variant="h1" fontWeight={400}>
+                    {unit === 'c' ? current.temp_c : current.temp_f}
+                  </Typography>
+                  <Typography variant="h3">
+                    {unit === 'c' ? '°C' : '°F'}
                   </Typography>
                 </Box>
-                <Box display="flex" alignItems="center" gap="10px" mb={2}>
-                  <Image
-                    src={`http:${current.icon}`}
-                    width={30}
-                    height={30}
-                    alt={current.weatherCondition}
-                  />
-                  <Typography fontSize="12px">
-                    Rain - {current.rainChance}%
-                  </Typography>
-                </Box>
-                <CityImage />
+
+                <Stack gap={3}>
+                  <Box display="flex" gap="5px">
+                    <Typography>{day},</Typography>
+                    <Typography color="grey">{time}</Typography>
+                  </Box>
+
+                  <Divider />
+                  <Box display="flex" alignItems="center" gap="10px">
+                    <Image
+                      src={`http:${current.icon}`}
+                      width={30}
+                      height={30}
+                      alt={current.weatherCondition}
+                    />
+                    <Typography fontSize="12px">
+                      {current.weatherCondition}
+                    </Typography>
+                  </Box>
+                  <Box display="flex" alignItems="center" gap="10px" mb={2}>
+                    <Image
+                      src={`http:${current.icon}`}
+                      width={30}
+                      height={30}
+                      alt={current.weatherCondition}
+                    />
+                    <Typography fontSize="12px">
+                      Rain - {current.rainChance}%
+                    </Typography>
+                  </Box>
+                  <CityImage />
+                </Stack>
+              </Grid2>
+            )}
+            <Grid2
+              size={11.5}
+              sx={{
+                padding: '3rem',
+                background: '#f5f4eb',
+                borderRadius: '0 25px 25px 0',
+              }}
+            >
+              <Stack gap={8}>
+                <Header />
+
+                <Stack gap={5}>
+                  <WeatherCard />
+
+                  <Highlights />
+                </Stack>
               </Stack>
             </Grid2>
-          )}
-          <Grid2
-            size={11.5}
-            sx={{
-              padding: '3rem',
-              background: '#f5f4eb',
-              borderRadius: '0 25px 25px 0',
-            }}
-          >
-            <Stack gap={8}>
-              <Header />
-
-              <Stack gap={5}>
-                <WeatherCard />
-
-                <Highlights />
-              </Stack>
-            </Stack>
           </Grid2>
-        </Grid2>
-      </Paper>
+        </Paper>
+      )}
     </Box>
   );
 }
